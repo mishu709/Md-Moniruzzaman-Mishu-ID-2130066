5b324e309c7fc55d3fe20c34f9f4b30bc7c1e845
Md. Moniruzzaman Mishu <mdmishu709@gmail.com>
2026-04-04 01:50:14 +0600

    Update question1.dart

--- question1.dart
+++ question1.dart
@@ -21,15 +21,18 @@ double calculateBMI(double weight, double height) {
 }
 
 // 3. Write a function called getGrade that takes a score (int) and returns a grade (String) based on:
-//    - 90-100: A
-//    - 80-89: B
-//    - 70-79: C
-//    - 60-69: D
-//    - Below 60: F
-// TODO: Implement the getGrade function
 String getGrade(int score) {
-  // TODO: Add your logic here
-  return "";
+  if (score >= 90) {
+    return "A";
+  } else if (score >= 80) {
+    return "B";
+  } else if (score >= 70) {
+    return "C";
+  } else if (score >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
 }
 
 void main() {
